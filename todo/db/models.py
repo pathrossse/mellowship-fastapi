a9e82458cc485c036f8ceb3c6a5135a7b64b9aee
PETER AJITH CHERIAN <98728306+pathrossse@users.noreply.github.com>
2025-05-19 23:18:13 +0530

    Update models.py

--- todo/db/models.py
+++ todo/db/models.py
@@ -1,25 +1,28 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
-from .database import Base
+from sqlmodel import SQLModel, Field, Column, DataTime, Relationship
+from typing import Optional, List
 from datetime import datetime
 from pydantic import EmailStr
 
-class Todo(Base):
+class Todo(SQLModel, table=true):
     __tablename__ = "todos"
-    id = Column(Integer, primary_key=True, index=True)
-    description = Column(String, index=True)
-    deadline = Column(DateTime)
-    done = Column(Boolean, default=False)
-    user_id = Column(Integer, ForeignKey("users.id"))
 
-class User(Base):
-    __tablename__ = "users"    
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
+    id: Optional[int] = Field(default=None, primary_key=True)
+    description: str = Field(index=True)
+    deadline: Optional[datetime] = Field(sa_column=Column(DateTime))
+    done: bool = Field(default=False)
+    user_id: Optional[int] = Field(default=None, foreign_key="users.id")
 
-class BlacklistedToken(Base):
+class User(SQLModel, table=true):
+    __tablename__ = "users"
+    
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    email: str = Field(unique=True, index=True)
+    password: str
+
+class BlacklistedToken(SQLModel, table=true):
     __tablename__ = "blacklisted_tokens"    
-    id = Column(Integer, primary_key=True, index=True)
-    token = Column(String, unique=True, index=True)
-    blacklisted_on = Column(DateTime, default=datetime.utcnow)
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    token: str = Field(unique=True, index=True)
+    blacklisted_on: datetime = Field(default_factory=datetime.utcnow, sa_column=Column(DateTime))
