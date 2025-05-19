8815901bb39cbeecbed493874a4c3ba25641969c
PETER AJITH CHERIAN <98728306+pathrossse@users.noreply.github.com>
2025-05-19 22:54:06 +0530

    Update and rename todo/models.py to todo/db/models.py

--- todo/db/models.py
+++ todo/db/models.py
@@ -1,10 +1,10 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 from .database import Base
 from datetime import datetime
+from pydantic import EmailStr
 
 class Todo(Base):
     __tablename__ = "todos"
-
     id = Column(Integer, primary_key=True, index=True)
     description = Column(String, index=True)
     deadline = Column(DateTime)
@@ -12,16 +12,14 @@ class Todo(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
 
 class User(Base):
-    __tablename__ = "users"
-    
+    __tablename__ = "users"    
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
 
 class BlacklistedToken(Base):
-    __tablename__ = "blacklisted_tokens"
-    
+    __tablename__ = "blacklisted_tokens"    
     id = Column(Integer, primary_key=True, index=True)
     token = Column(String, unique=True, index=True)
     blacklisted_on = Column(DateTime, default=datetime.utcnow)
